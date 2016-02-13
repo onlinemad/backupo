d87bc00df42c7568b07fa8cada521ae3831aacc6
Ian Wu <onlinemad@gmail.com>
2016-02-13 17:51:06 +0800

    Supported gizp in ostar.js

--- plan/ostar.js
+++ plan/ostar.js
@@ -1,15 +1,24 @@
 /**
  * ostar.js
  *
- * OS build-in tar command. 
+ * OS build-in tar command.
  */
 var exec = require('child_process').exec,
   os = require('os'),
   debug = require('debug')('backupo');
 module.exports.backup = function(option, cb) {
+  var tar_option = 'cf';
   var file = os.tmpdir() + Date.now() + '.tar';
   debug('ostar.js', file);
-  var command = 'tar cf ' + file + ' ' + option.src;
+  if(option.option) {
+    tar_option = option.option;
+    if(tar_option.includes('z')){
+      file += '.gz';
+    } else if(tar_option.includes('j')){
+      file += '.bz';
+    }
+  }
+  var command = 'tar ' + tar_option + ' ' + file + ' ' + option.src;
   debug('ostar.js', command);
   var child = exec(command, function(error, stdout, stderr) {
     debug('ostar.js', 'stdout', stdout);
--- test/plan/ostar.test.js
+++ test/plan/ostar.test.js
@@ -3,10 +3,25 @@ var plan = require('../../plan/ostar');
 
 describe('Unit Test. Only test functionality of each method', function() {
   describe('#backup()', function() {
-    it('should make a backup by compress.js', function(done) {
-      plan.backup(null, function(err, filepath) {
+    it('should make a backup by ostar.js', function(done) {
+      var option = {
+        src: 'ostar'
+      }
+      plan.backup(option, function(err, filepath) {
         if (err) throw err;
         console.log(filepath);
+        done();
+      });
+    });
+    it('should make a backup by ostar.js', function(done) {
+      var option = {
+        src: 'ostar',
+        option: 'czf'
+      }
+      plan.backup(option, function(err, filepath) {
+        if (err) throw err;
+        var stat = fs.statSync(filepath);
+        expect(stat.isFile()).to.be.true;
         done();
       });
     })
